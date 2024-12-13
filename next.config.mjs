86b637d4f1fee414beea4eeaa60475871e68d4e6
Maxime Dénervaud <maxime.denervaud@students.hevs.ch>
2024-12-13 08:09:22 +0100

    Edit host name

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,7 @@ const nextConfig = {
       },
       {
         protocol: "https",
-        hostname: "https://archecollectif-back.onrender.com/",
+        hostname: "https://admin.larchcollectif.ch/",
         pathname: "/uploads/**",
       },
     ],
