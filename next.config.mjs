28dc74913c43caa32bf03799816482163abfd42f
Maxime Dénervaud <maxime.denervaud@students.hevs.ch>
2024-12-13 08:59:28 +0100

    bigfix

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,7 @@ const nextConfig = {
       },
       {
         protocol: "https",
-        hostname: "https://admin.larchcollectif.ch/",
+        hostname: "https://admin.larchcollectif.ch",
         pathname: "/uploads/**",
       },
     ],
